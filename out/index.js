4b3f2a108a5f15390aa53fc534c500a2681158be
Who5673 <windows7modrenedition@gmail.com>
2025-07-06 07:50:23 +0700

    coc-who5673-nasm commit

--- out/index.js
+++ out/index.js
@@ -21,6 +21,16 @@ class NasmCompletionProvider {
                 detail: 'Jump to a label',
             },
             {
+                label: 'jnz',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Jump to a label if ZF != 0',
+            },
+            {
+                label: 'syscall',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: "Call the system or (or and) kernel"
+            },
+            {
                 label: 'inc',
                 kind: coc_nvim_1.CompletionItemKind.Keyword,
                 detail: 'Increment operand (inc something = something += 1)',
@@ -54,6 +64,76 @@ class NasmCompletionProvider {
                 label: 'rbx',
                 kind: coc_nvim_1.CompletionItemKind.Keyword,
                 detail: 'Register RBX',
+            },
+            {
+                label: 'add',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Add a value into a target (example: add [target], 2 means [target] += 2).\nTarget can be a register.',
+            },
+            {
+                label: 'sub',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Subtract a value into a target (example: sub [target], 2 means [target] += 2).',
+            },
+            {
+                label: 'db',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Define byte a value (1 byte) for a character, array of characters or short numbers',
+            },
+            {
+                label: 'dw',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Define word (2 bytes) for 16-bit short int',
+            },
+            {
+                label: 'dd',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Define double word (4 bytes) for 32-bit int',
+            },
+            {
+                label: 'dq',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Define quad word (8 bytes) for 64-bit int',
+            },
+            {
+                label: 'dt',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Define ten bytes (10 bytes) for extended real number (x87 float)'
+            },
+            {
+                label: 'equ',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Define a constant',
+            },
+            {
+                label: '.data',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: '.data section for defining some labels',
+            },
+            {
+                label: '.text',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: '.text section',
+            },
+            {
+                label: '.bss',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: '.bss section for creating a memory for reading (buffer)',
+            },
+            {
+                label: 'resb',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Define and use memory for non-initialized data (avoid buffer overflow while reading something)',
+            },
+            {
+                label: 'global',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Global a label of a program (example: global _start)',
+            },
+            {
+                label: '_start',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'The label for nasm to start the program.'
             }
         ];
     }
--- src/index.ts
+++ src/index.ts
@@ -42,6 +42,16 @@ class NasmCompletionProvider implements CompletionItemProvider {
         detail: 'Jump to a label',
       },
       {
+        label: 'jnz',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Jump to a label if ZF != 0',
+      },
+      {
+        label: 'syscall',
+        kind: CompletionItemKind.Keyword,
+        detail: "Call the system or (or and) kernel"
+      },
+      {
         label: 'inc',
         kind: CompletionItemKind.Keyword,
         detail: 'Increment operand (inc something = something += 1)',
@@ -75,6 +85,76 @@ class NasmCompletionProvider implements CompletionItemProvider {
         label: 'rbx',
         kind: CompletionItemKind.Keyword,
         detail: 'Register RBX',
+      },
+      {
+        label: 'add',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Add a value into a target (example: add [target], 2 means [target] += 2).\nTarget can be a register.',
+      },
+      {
+        label: 'sub',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Subtract a value into a target (example: sub [target], 2 means [target] += 2).',
+      },
+      {
+        label: 'db',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Define byte a value (1 byte) for a character, array of characters or short numbers',
+      },
+      {
+        label: 'dw',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Define word (2 bytes) for 16-bit short int',
+      },
+      {
+        label: 'dd',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Define double word (4 bytes) for 32-bit int',
+      },
+      {
+        label: 'dq',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Define quad word (8 bytes) for 64-bit int',
+      },
+      {
+        label: 'dt',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Define ten bytes (10 bytes) for extended real number (x87 float)'
+      },
+      {
+        label: 'equ',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Define a constant',
+      },
+      {
+        label: '.data',
+        kind: CompletionItemKind.Keyword,
+        detail: '.data section for defining some labels',
+      },
+      {
+        label: '.text',
+        kind: CompletionItemKind.Keyword,
+        detail: '.text section',
+      },
+      {
+        label: '.bss',
+        kind: CompletionItemKind.Keyword,
+        detail: '.bss section for creating a memory for reading (buffer)',
+      },
+      {
+        label: 'resb',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Define and use memory for non-initialized data (avoid buffer overflow while reading something)',
+      },
+      {
+        label: 'global',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Global a label of a program (example: global _start)',
+      },
+      {
+        label: '_start',
+        kind: CompletionItemKind.Keyword,
+        detail: 'The label for nasm to start the program.'
       }
     ];
   }
