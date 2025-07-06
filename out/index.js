7e82c40f63d21dcf0ce8e3aacc75951c12b56d23
Who5673 <windows7modrenedition@gmail.com>
2025-07-06 07:08:35 +0700

    coc-who5673-nasm commit

--- out/index.js
+++ out/index.js
@@ -1,7 +1,60 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.activate = void 0;
+const coc_nvim_1 = require("coc.nvim");
 async function activate(context) {
     console.log('coc-who5673-snippets activated!');
+    context.subscriptions.push(coc_nvim_1.languages.registerCompletionItemProvider('coc-nasm', 'nasm', ['nasm'], new NasmCompletionProvider(), ['']));
 }
 exports.activate = activate;
+class NasmCompletionProvider {
+    async provideCompletionItems(document, position) {
+        return [
+            {
+                label: 'mov',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Move data from source to destination (mov [destination], [source].)',
+            },
+            {
+                label: 'jmp',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Jump to a label',
+            },
+            {
+                label: 'inc',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Increment operand (inc something = something += 1)',
+            },
+            {
+                label: 'eax',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Register EAX',
+            },
+            {
+                label: 'rax',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Register RAX (needed to call the syscall number or return a value in a function)',
+            },
+            {
+                label: 'rdi',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Register RDI (needed to read the file descriptor of a file (Hint: Terminal screen = stdout = 1))',
+            },
+            {
+                label: 'rsi',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Register RSI',
+            },
+            {
+                label: 'rdx',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Register RDX',
+            },
+            {
+                label: 'rbx',
+                kind: coc_nvim_1.CompletionItemKind.Keyword,
+                detail: 'Register RBX',
+            }
+        ];
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,81 @@
-import { ExtensionContext } from 'coc.nvim';
+import { 
+  ExtensionContext,
+  languages,
+  CompletionItem,
+  CompletionItemKind,
+  CompletionItemProvider,
+  Position,
+  TextDocument,
+  CancellationToken,
+  CompletionContext,
+  CompletionList,
+  ProviderResult,
+} from 'coc.nvim';
 
 export async function activate(context: ExtensionContext): Promise<void> {
   console.log('coc-who5673-snippets activated!');
+  context.subscriptions.push(
+    languages.registerCompletionItemProvider(
+      'coc-nasm',
+      'nasm',
+      ['nasm'],
+      new NasmCompletionProvider(),
+      [''],
+    )
+  );
+}
+
+class NasmCompletionProvider implements CompletionItemProvider {
+  async provideCompletionItems(
+    document: TextDocument,
+    position: Position
+  ): Promise<CompletionItem[]> {
+    return [
+      {
+        label: 'mov',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Move data from source to destination (mov [destination], [source].)',
+      },
+      {
+        label: 'jmp',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Jump to a label',
+      },
+      {
+        label: 'inc',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Increment operand (inc something = something += 1)',
+      },
+      {
+        label: 'eax',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Register EAX',
+      },
+      {
+        label: 'rax',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Register RAX (needed to call the syscall number or return a value in a function)',
+      },
+      {
+        label: 'rdi',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Register RDI (needed to read the file descriptor of a file (Hint: Terminal screen = stdout = 1))',
+      },
+      {
+        label: 'rsi',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Register RSI',
+      },
+      {
+        label: 'rdx',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Register RDX',
+      },
+      {
+        label: 'rbx',
+        kind: CompletionItemKind.Keyword,
+        detail: 'Register RBX',
+      }
+    ];
+  }
 }
