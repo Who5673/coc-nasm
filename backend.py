ed9b43c9298d22eeaf641d4efcfd86461f81c317
Who5673 <windows7modrenedition@gmail.com>
2025-07-08 15:49:00 +0700

    coc-who5673-nasm commit

--- backend.py
+++ backend.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="docs")
+result="Hello"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", message=f"{result}")
 
 if __name__ == "__main__":
     app.run(debug=True)
