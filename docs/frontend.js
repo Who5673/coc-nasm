57feb035948e51fc936e83a3dde390fa14a8fe67
Who5673 <windows7modrenedition@gmail.com>
2025-07-15 18:26:24 +0700

    website commit

--- docs/frontend.js
+++ docs/frontend.js
@@ -7,26 +7,40 @@ Module.onRuntimeInitialized = () => {
 
   /* Static scripts */
   const backgroundmode = document.getElementById("light");
+  const menu = document.getElementsByTagName("nav")[0];
   backgroundmode.addEventListener("click", () => {
     switch (current_background) {
       case "dark":
         document.getElementById("bodie").classList.add("light");
+        menu.classList.add("light");
         current_background = returnformat("light");
         break;
       default:
         document.getElementById("bodie").classList.remove("light");
+        menu.classList.remove("light");
         current_background = returnformat("dark");
         break;
     }
   });
 
-  /* JavaScript Copy Clipboard */
-  let cpclipbd = document.querySelectorAll(".c2");
-  cpclipbd.forEach((btn, idx) => {
-    btn.addEventListener("click", () => {
-      const code = document.querySelectorAll(".codeblock")[idx].textContent;
-      let cpresult = navigator.clipboard.writeText(code);
-      cpresult.then(() => alert("Copied")).catch(() => alert("Error while copying"));
-    });
+  /* Get the boxes in Home */
+  document.getElementById("getlinknow1").addEventListener("click", () => {
+    window.location.href = "installation_for_coc.html";
+  });
+  document.getElementById("getlinknow2").addEventListener("click", () => {
+    window.location.href = "installation_for_nvim.html";
+  });
+
+
+  /* Getlink in Home */
+  let getlinknow3, getlinknow4, getlinknow5, getlinknow6;
+  copystatus = document.getElementsByClassName("c2");
+  getlinknow3 = document.getElementById("getlinknow3");
+  getlinknow6 = document.getElementById("getlinknow6");
+  getlinknow4 = document.getElementById("getlinknow4");
+  getlinknow5 = document.getElementById("getlinknow5");
+  
+  getlinknow6.addEventListener("click", () => {
+    window.location.href = "fake_document.html";
   });
 }
---
+++ docs/highlighters/tshighlight/DIGESTS.md
@@ -0,0 +1,29 @@
+## Subresource Integrity
+
+If you are loading Highlight.js via CDN you may wish to use [Subresource Integrity](https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity) to guarantee that you are using a legimitate build of the library.
+
+To do this you simply need to add the `integrity` attribute for each JavaScript file you download via CDN. These digests are used by the browser to confirm the files downloaded have not been modified.
+
+```html
+<script
+  src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/highlight.min.js"
+  integrity="sha384-5xdYoZ0Lt6Jw8GFfRP91J0jaOVUq7DGI1J5wIyNi0D+eHVdfUwHR4gW6kPsw489E"></script>
+<!-- including any other grammars you might need to load -->
+<script
+  src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/languages/go.min.js"
+  integrity="sha384-HdearVH8cyfzwBIQOjL/6dSEmZxQ5rJRezN7spps8E7iu+R6utS8c2ab0AgBNFfH"></script>
+```
+
+The full list of digests for every file can be found below.
+
+### Digests
+
+```
+sha384-Z61gsCS2W7Q+3fT1fdya/Sz4wlmsotT9iEGzgIlNqP0soaKH1NzysutxWp08Hh3E /es/languages/typescript.js
+sha384-Tv4mr9B7b+x2IynRXW/xcAxUj1+AoN9zyp0n9BWE1Nle3Zfm/zUeEztNLhIRjgE7 /es/languages/typescript.min.js
+sha384-8v3YMaXFO9cmTNxsHWqwn9wJsV1jVO7rwx4huxqlEQpT/P2tuDbtm+Hs0EdYqu0a /languages/typescript.js
+sha384-df1w1nJ43GNwmgbSCrT8YFIYyqFAm+lzj+b6ofuziX8Cfdg9QHFwbORDgAaj//wi /languages/typescript.min.js
+sha384-iqknFGgo+b2ddwE33HkMENwhax6iAX8ZqUntfKzsK7OFXIUrd0kjpODCaFRsAoH6 /highlight.js
+sha384-pFZayhhVxN4I2NWV/v3/e9+VeUJaaalul23ZAELKBwmj5UAX3Ban+R4nuBtJaDOd /highlight.min.js
+```
+
